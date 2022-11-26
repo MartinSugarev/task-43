7c7470f2d7293c1763aaac7b34066775265a151d
Martin <mdsugarev@gmail.com>
2022-11-27 01:17:37 +0200

    add new title after 3 seconds

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,9 @@
-import "../scss/app.scss";
-
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+setTimeout(() => {
+ let title =   document.querySelector('title')
+ title.textContent = 'One new message'
+}, 3000)
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
